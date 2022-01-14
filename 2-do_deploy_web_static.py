f1aafa9d53ee87c38d2fe0c1d59642b7553ef4f9
IsabelaGomez101 <isa.gomc@gmail.com>
2022-01-13 23:51:18 -0500

    adding file do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,16 +6,16 @@ env.hosts = ['35.243.238.95', '3.91.82.101']
 
 
 def do_deploy(archive_path):
-    ''''''
+    '''method do deploy'''
     if os.path.isfile(archive_path) is False:
         return False
-    filename = archive_path.split('/')[-1]
-    archive = filename.split('.')[0]
-    path = "/data/web_static/releases/{}/".format(archive)
     try:
+        filename = archive_path.split('/')[-1]
+        archive = filename.split('.')[0]
+        path = "/data/web_static/releases/{}/".format(archive)
         put("{}".format(archive_path), "/tmp/{}".format(filename))
-        run("mkdir -p ".format(path))
-        run("tar -xzf /tmp/{} -C {}".format(path))
+        run("mkdir -p {}".format(path))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path))
         run("rm /tmp/{}".format(filename))
         run("mv {}web_static/* {}".format(path, path))
         run("rm -rf {}web_static".format(path))
