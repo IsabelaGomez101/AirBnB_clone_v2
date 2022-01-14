5d5dd31191c0ba0f9b340fd13a0cb42e77563b30
IsabelaGomez101 <isa.gomc@gmail.com>
2022-01-13 23:43:00 -0500

    adding file do deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# Fabric script that distributes an archive to your web servers
+from fabric.api import *
+import os
+env.hosts = ['35.243.238.95', '3.91.82.101']
+
+
+def do_deploy(archive_path):
+    ''''''
+    if os.path.isfile(archive_path) is False:
+        return False
+    filename = archive_path.split('/')[-1]
+    archive = filename.split('.')[0]
+    path = "/data/web_static/releases/{}/".format(archive)
+    try:
+        put("{}".format(archive_path), "/tmp/{}".format(filename))
+        run("mkdir -p ".format(path))
+        run("tar -xzf /tmp/{} -C {}".format(path))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(path, path))
+        run("rm -rf {}web_static".format(path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(path))
+        return True
+    except:
+        return False
