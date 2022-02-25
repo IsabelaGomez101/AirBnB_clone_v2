e60b678de03dc3e7e426bd2649cb6ab32447611c
IsabelaGomez101 <isa.gomc@gmail.com>
2022-02-24 21:11:40 -0500

    adding file hbnb and fix hello route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,6 +14,13 @@ def hello_HBNB():
     """
     return "Hello HBNB!"
 
+@app.route('/hbnb', strict_slashes=False)
+def display_HBNB():
+    """
+    display “HBNB”
+    """
+    return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
